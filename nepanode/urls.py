5c2c861849e7d57b4abbe8d4eb36d17e1239a047
DOE-NEPA <john.jediny@hq.doe.gov>
2015-02-14 05:08:55 +0000

    add .gitignore for local_settings file

--- nepanode/urls.py
+++ nepanode/urls.py
@@ -4,7 +4,20 @@ from django.views.generic import TemplateView
 from geonode.urls import *
 
 urlpatterns = patterns('',
+   #Third Party Apps
+   #Zinnia
+   url(r'^pages/', include('zinnia.urls', namespace='zinnia')),
+   url(r'^pages/comments/', include('django_comments.urls')),
+   url(r'^tinymce/zinnia/', include('zinnia_tinymce.urls')),
+   (r'^tinymce/', include('tinymce.urls')),
+
+   #Django-Helpdesk
+   (r'forms/', include('helpdesk.urls')),
+
+   #Geonode Project
    url(r'^/?$',
        TemplateView.as_view(template_name='site_index.html'),
        name='home'),
- ) + urlpatterns
+   url(r'', include('social.apps.django_app.urls', namespace='social'))
+) + urlpatterns
+
